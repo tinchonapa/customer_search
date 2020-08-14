7c2636422b5d38739e616f9782480fc8f1824538
Martin Naparstek <tinchonapa@gmail.com>
2020-08-14 10:33:51 -0600

    Updates

---
+++ precache-manifest.ba135f8c2593bbb656d5a6d79fbb8eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef0da4fb411d986f419a4becb0d4ba5",
+    "url": "/customer_search/index.html"
+  },
+  {
+    "revision": "0c8713be2fad85814d8e",
+    "url": "/customer_search/static/css/main.a8780133.chunk.css"
+  },
+  {
+    "revision": "6c722b180bbb67214e44",
+    "url": "/customer_search/static/js/2.37e47af5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/customer_search/static/js/2.37e47af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8713be2fad85814d8e",
+    "url": "/customer_search/static/js/main.bfe3f481.chunk.js"
+  },
+  {
+    "revision": "fcd351979884d7896ae6",
+    "url": "/customer_search/static/js/runtime-main.94401a20.js"
+  }
+]);
